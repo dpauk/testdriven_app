d893fc2990b130e504029ea63e4ac1e9f105faa4
David Albone <davd.p.albone@gmail.com>
2018-04-22 15:40:33 +0100

    completed part2 - code coverage and quality

--- services/users/manage.py
+++ services/users/manage.py
@@ -2,10 +2,21 @@
 
 import unittest
 
+import coverage
 from flask.cli import FlaskGroup
 
 from project import create_app, db
 from project.api.models import User
+
+COV = coverage.coverage(
+    branch=True,
+    include='project/*',
+    omit=[
+        'project/tests/*',
+        'project/config.py',
+    ]
+)
+COV.start()
 
 app = create_app()
 cli = FlaskGroup(create_app=create_app)
@@ -34,6 +45,22 @@ def seed_db():
     db.session.add(User(username='david', email='david@example.com'))
     db.session.add(User(username='joebloggs', email='joebloggs@example.com'))
     db.session.commit()
+
+
+@cli.command()
+def cov():
+    """Runs the unit tests with coverage."""
+    tests = unittest.TestLoader().discover('project/tests')
+    result = unittest.TextTestRunner(verbosity=2).run(tests)
+    if result.wasSuccessful():
+        COV.stop()
+        COV.save()
+        print("Coverage summary:")
+        COV.report()
+        COV.html_report()
+        COV.erase()
+        return 0
+    return 1
 
 
 if __name__ == '__main__':
--- services/users/project/tests/test_users.py
+++ services/users/project/tests/test_users.py
@@ -54,7 +54,8 @@ class TestUserService(BaseTestCase):
             self.assertIn('fail', data['status'])
 
     def test_add_user_invalid_json_keys(self):
-        """Ensure error is thrown if the JSON object does not have a username key"""
+        """Ensure error is thrown if the JSON object
+        does not have a username key"""
         with self.client:
             response = self.client.post(
                 '/users',
@@ -79,7 +80,7 @@ class TestUserService(BaseTestCase):
             )
             response = self.client.post(
                 '/users',
-                data = json.dumps({
+                data=json.dumps({
                     'username': 'david',
                     'email': 'david@example.com'
                 }),
@@ -87,7 +88,8 @@ class TestUserService(BaseTestCase):
             )
             data = json.loads(response.data.decode())
             self.assertEqual(response.status_code, 400)
-            self.assertIn('Sorry.  That email already exists.', data['message'])
+            self.assertIn('Sorry.  That email already exists.',
+                          data['message'])
             self.assertIn('fail', data['status'])
 
     def test_single_user(self):
@@ -129,20 +131,25 @@ class TestUserService(BaseTestCase):
             self.assertEqual(response.status_code, 200)
             self.assertEqual(len(data['data']['users']), 2)
             self.assertIn('david', data['data']['users'][0]['username'])
-            self.assertIn('david@example.com', data['data']['users'][0]['email'])
-            self.assertIn('joebloggs', data['data']['users'][1]['username'])
-            self.assertIn('joebloggs@example.com', data['data']['users'][1]['email'])
+            self.assertIn('david@example.com',
+                          data['data']['users'][0]['email'])
+            self.assertIn('joebloggs',
+                          data['data']['users'][1]['username'])
+            self.assertIn('joebloggs@example.com',
+                          data['data']['users'][1]['email'])
             self.assertIn('success', data['status'])
 
     def test_main_no_users(self):
-        """Ensure the main route behaves correctly when no users have been added to the database"""
+        """Ensure the main route behaves correctly when no users
+        have been added to the database"""
         response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
         self.assertIn(b'<h1>All Users</h1>', response.data)
         self.assertIn(b'p>No users!</p>', response.data)
 
     def test_main_with_users(self):
-        """Ensure the main route behaves correctly when users have been added to the database"""
+        """Ensure the main route behaves correctly when users
+        have been added to the database"""
         add_user('david', 'david@example.com')
         add_user('joebloggs', 'joebloggs@example.com')
         with self.client:
